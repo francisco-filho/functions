3e235555826a65e5948075ca59525b3e4e06c4b8
Francisco Filho <francico@capimgrosso.com>
2025-04-29 18:53:11 -0300

    Change response to a dict

--- function_app.py
+++ function_app.py
@@ -20,7 +20,4 @@ def hello(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse(json.dumps(dict(message=f"Hello, {name}")), status_code=200)

041b636e750e218f2acd2b62a7cef43b2edcace5
Francisco Filho <francico@capimgrosso.com>
2025-04-30 14:45:59 -0300

    fix function name

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ import logging
 app = func.FunctionApp()
 
 @app.route(route="hello2", auth_level=func.AuthLevel.ANONYMOUS)
-def hello(req: func.HttpRequest) -> func.HttpResponse:
+def hello2(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     name = req.params.get('name')
     if not name:
